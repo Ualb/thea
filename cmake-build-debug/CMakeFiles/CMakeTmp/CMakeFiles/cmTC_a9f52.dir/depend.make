12429a8f9c9d39f83e47771975fab3e6974ad9ba
Ulises López <ulises050794@gmail.com>
2020-08-22 22:49:04 -0600

    cmake added

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_a9f52.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_a9f52.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/Theia.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Theia.dir/theia.c.obj"
+  "Theia.exe"
+  "Theia.exe.manifest"
+  "Theia.lib"
+  "Theia.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Theia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
