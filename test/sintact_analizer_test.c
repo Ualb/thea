25348b6caaa9ca2c0c3222aea307468f53bced4e
Ulises López <ulises050794@gmail.com>
2020-09-06 19:57:37 -0600

    test para remove_end_spaces

--- test/sintact_analizer_test.c
+++ test/sintact_analizer_test.c
@@ -5,17 +5,24 @@
 #include "../utility/utility.h"
 
 boolean test_subtract_text();
+boolean test_remove_end_spaces();
 
 boolean main(void) {
     printf("%s", test_subtract_text()? "." : "F");
+    printf("%s", test_remove_end_spaces()? "." : "F");
     return EXIT_SUCCESS;
 }
 
 boolean test_subtract_text() {
     char *text = "hola mundo cruel";
     char *data = substract_text(text, 5, (int) (strlen(text)));
-    if(strcasecmp(data, "mundo cruel") == 0) {
-        return True;
-    }
+    if(strcasecmp(data, "mundo cruel") == 0) return True;
+    return False;
+}
+
+boolean test_remove_end_spaces() {
+    char *text = "esto   ";
+    char *data = remove_end_spaces(text);
+    if(strcasecmp(data, "esto") == 0) return True;
     return False;
 }
