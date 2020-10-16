927d8792804e2a7b729f0952d02c0719ba9724eb
Ulises López <ulises050794@gmail.com>
2020-10-16 13:32:41 -0600

    advance in createControlDOC, for the ".equifinality" file

--- src/equifinality/iofile.c
+++ src/equifinality/iofile.c
@@ -1,45 +1,60 @@
 #include "iofile.h"
 #include "../constants.h"
 #include "../errors/panic.h"
+#include "../engine_string/analizer.h"
 
+#include <stdio.h>
 #include <dirent.h>
 #include <errno.h>
+#include <string.h>
 
-boolean fairlureStreamFile(const char* location);
-
+boolean okStreamFile(const char* location);
+boolean createControlDOC(const char* name);
+char* getCurrentPath(void);
 
 boolean createDOC(const char* name, const char* location) {
 //    first check the location
-    if (fairlureStreamFile(location)) {
+    if (okStreamFile(location)) {
 //        the create a file functionality
+        FILE *file = fopen(location, "rb+");
+        if (file == NULL) return False;
+//        to create the internal procedures
+        return createControlDOC(name);
     }
-    return True;
+
+    return False;
 }
 
 /**
  * The algoritm creates a file in the current path with the name
  * '.equifinality' and intoduce the dir of the docs created's.
- *
+ * @param struct of FILE
+ * @return Success (True) or fairlure (False)
  */
-boolean createControlDOC() {
+boolean createControlDOC(const char* name) {
+    char *path = getCurrentPath();
+    FILE *file = fopen(strcat(path, name), "r");
+    if (file == NULL) return False;
+//    write datas in the file
     return True;
 }
 
 /**
  * The algoritm updates a file in the current path with the name
  * '.equifinality', see @relatesalso 'createControlDOC'.
- *
+ * @param struct of FILE
+ * @return Success (True) or fairlure (False)
  */
-boolean updateControlDOC() {
+boolean updateControlDOC(const char* name) {
     return True;
 }
 
 /**
- * Check so the dir of the DOC is ok.
+ * Check so the dir of the DOC is input/output ok.
  * @param location Dir to check
- * @return Success or fairlure
+ * @return Success (True) or fairlure (False)
  */
-boolean fairlureStreamFile(const char* location) {
+boolean okStreamFile(const char* location) {
     DIR *dir;
     dir = opendir(location);
     if (dir) {
@@ -50,4 +65,15 @@ boolean fairlureStreamFile(const char* location) {
     }
 
     return False;
+}
+
+char* getCurrentPath(void) {
+    FILE *file = fopen("./current.txt", "r");
+    if (file == NULL) panic(errno);
+    char *buffer[150];
+    while(feof(file) == 0) {
+        fgets(*buffer, 150, file);
+        if (startWith(buffer, "CURRENT_PATH") == True) return substractSText(*buffer, "=", "");
+    }
+    return "";
 }
